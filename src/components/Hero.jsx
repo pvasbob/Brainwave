972e6adba14f27e94d8a83946dd803f50388aa02
pvasbob <pvasbob@gmail.com>
2025-12-06 18:51:55 -0500

    Add Generating

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants/index";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import Generating from "./Generating";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -54,6 +55,9 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex ">
                     {heroIcons.map((icon, index) => (
@@ -80,6 +84,7 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
       </div>
+      <BottomLine />
     </Section>
   );
 };
