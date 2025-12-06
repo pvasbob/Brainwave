cb70147103206be53367cf3a56fb1d9ed2dc21c1
pvasbob <pvasbob@gmail.com>
2025-12-06 07:44:35 -0500

    Add Sign up button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `btn relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+  const classes = `btn uppercase font-code  relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { brainwave } from "../assets";
 import { navigation } from "../constants/index.js";
 import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
 const Header = () => {
   const pathname = useLocation();
   return (
     <div
-      className="fixed top-0 z-50 bg-n-8/90
+      className="fixed top-0 left-0 w-full z-50 bg-n-8/90
   backdrop-blur-sm  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm"
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
@@ -32,6 +33,16 @@ const Header = () => {
             ))}
           </div>
         </nav>
+
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        >
+          New Account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign in
+        </Button>
       </div>
     </div>
   );
