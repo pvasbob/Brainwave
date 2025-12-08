727aa4b80f219e80b816b3c6602b20bc4be63655
pvasbob <pvasbob@gmail.com>
2025-12-08 13:14:54 -0500

    Add Code Generation bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ import { heroIcons } from "../constants/index";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
+import Notification from "./Notification";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -66,6 +67,12 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
+                </ScrollParallax>
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
                 </ScrollParallax>
               </div>
             </div>
